9608569d387ff310599f2606606e2fe9c738c3f4
Justin Ramsay <jramsay42@gmail.com>
2017-11-24 21:13:34 +1000

    Added precondtions to gun

--- Gun.py
+++ Gun.py
@@ -21,7 +21,7 @@ class Gun(object):
 
             Preconditons:
                 0 < elevation < 90
-                0 < azimuth < 90
+                0 < azimuth < 360
                 0 < ammo
         """
         self.x = x
@@ -37,14 +37,27 @@ class Gun(object):
         return self.elevation
 
     def calculate_range(self):
+        """ Returns the linear range of the gun at the current elevation. """
         return EXIT_VELOCITY ** 2 * sin(2 * radians(self.elevation)) / GRAVITY 
 
     def set_azimuth(self, azimuth):
+        """ Precondition: 0 <= azimuth < 360 """
         self.azimuth = azimuth	
 
     def set_elevation(self, elevation):
+        """ Precondition: 0 < elevation < 90 """
         self.elevation = elevation
 
+    def shoot_gun(self):
+        """ Returns True if the gun has ammo and updates the ammo. Otherwise,
+            False is returned. """
+        if (self.ammo > 0 ):
+            self.ammo -= 1
+            return True
+        else:
+            return False
+
     def move(self, x, y):
+        """ Precondition: 0 <= x <= MAX_X, 0 <= y <= MAX_Y """
         self.x = x
         self.y = y
