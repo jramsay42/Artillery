dd62bb5fc4c35618186ef60a8aa37b5f6aec53b6
Justin Ramsay <jramsay42@gmail.com>
2017-11-24 21:14:12 +1000

    Added comments to player interface

--- PlayerInterface.py
+++ PlayerInterface.py
@@ -5,4 +5,9 @@ clientsocket.connect(('localhost', 31415))
 clientsocket.send('hello'.encode('utf-8'))
 
 class PlayerInterface(object):
+    """
+        Class for handling player interactions with the server. Reads messages 
+        from the server and displays them to the player. Also sends responses
+        back to the server.
+    """
     pass
